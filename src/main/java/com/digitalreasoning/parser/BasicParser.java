2ab5136ce621a2650dfff2dac29ff4c40e192e26
Jesse Lindsey <jesselindsey@gmail.com>
2016-09-21 23:32:55 -0400

    Modifed parser to handle nuance period cases

--- src/main/java/com/digitalreasoning/parser/BasicParser.java
+++ src/main/java/com/digitalreasoning/parser/BasicParser.java
@@ -6,6 +6,8 @@ import com.digitalreasoning.entities.Token;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by lindsey on 9/20/16.
@@ -14,13 +16,13 @@ public class BasicParser {
 
 
 
-    String punctuation = "()\"?,'";
+    String punctuation = "()\"?,':";
 
     enum TokenType{
-        EMPTY,WHITESPACE,DOT,PUNCTUATION,CHARACTER, IGNORE;
+        EMPTY,WHITESPACE,DOT,PUNCTUATION,CHARACTER, IGNORE, NUMBER;
 
         public boolean isTokenizeAble() {
-            return this==DOT || this==PUNCTUATION || this==CHARACTER;
+            return this==DOT || this==PUNCTUATION || this==CHARACTER || this == NUMBER;
         }
     }
 
@@ -46,7 +48,11 @@ public class BasicParser {
         TokenType lastTokenType     = TokenType.EMPTY;
         TokenType currentTokenType  = TokenType.EMPTY;
         int data = inputStreamReader.read();
-         while (data > -1) {
+        boolean reachedEOS = false;
+         while (!reachedEOS) {
+             if (data < 0){
+                 reachedEOS = true;
+             }
             char c = (char) data;
 
             // determine the token type
@@ -58,52 +64,123 @@ public class BasicParser {
                 currentTokenType = TokenType.WHITESPACE;
             } else if (punctuation.indexOf(c) >= 0){
                 currentTokenType = TokenType.PUNCTUATION;
+            } else if (Character.isDigit(c)) {
+                currentTokenType = TokenType.NUMBER;
             } else {
                 currentTokenType = TokenType.CHARACTER;
             }
 
-            if (lastTokenType == currentTokenType   ){
-                // Case the the token type is the same
-                if (currentTokenType.isTokenizeAble()) {
-                    if (currentTokenType == TokenType.PUNCTUATION) {
-                        // each punctuation is duplicated
-                        saveNonZeroLengthTokens(currentSentence, currentToken);
-                    }
-                    currentToken.append(c);
-                }
-            } else {
-                // Case the the token type changes
-                if  (lastTokenType.isTokenizeAble() ) {
-                    if (lastTokenType != TokenType.DOT  ||
-                        lastTokenType == TokenType.DOT && currentToken.length() > 1){
-                        saveNonZeroLengthTokens(currentSentence, currentToken);
-                    }else {
-                        // start a new sentence
-                        if (currentSentence.getTokens().size() > 0){
-                            sentences.add(currentSentence);
-                            currentSentence = new Sentence();
-                        }
-                    }
 
-                }
+            if (reachedEOS || lastTokenType != TokenType.WHITESPACE && currentTokenType == TokenType.WHITESPACE && currentToken.length() > 0){
+                // we are at a whitespace break
+
+                String tokenStr = currentToken.toString();
                 currentToken.setLength(0);
-                currentToken.append(c);
-                lastTokenType = currentTokenType;
-            }
+                if (tokenStr.matches("^\\w+$")) {
+                    /*word*/
+                    currentSentence.getTokens().add(new Token(tokenStr));
+                } else if (tokenStr.matches("^\\w+'\\w+$")){
+                    /*word's*/
+                        currentSentence.getTokens().add( new Token(tokenStr));
+                } else if (tokenStr.matches("^\\d+\\.\\d+$")){
+                    /*2.12*/
+                    currentSentence.getTokens().add( new Token(tokenStr));
+                } else if (tokenStr.matches("(\\w+)(\\.)(\\w+)")){
 
+                    /*word.word*/
+                    currentSentence.getTokens().add( new Token(tokenStr) );
+
+
+                } else if (tokenStr.matches("^\\w+\\.$")){
+                    currentSentence.getTokens().add( new Token(tokenStr.substring(0,tokenStr.length() - 1)));
+                    currentSentence.getTokens().add( new Token("."));
+                    if (currentSentence.getTokens().size() > 0){
+                        sentences.add(currentSentence);
+                        currentSentence = new Sentence();
+                    }
+
+                } else if (tokenStr.matches("^(\\w+)(\\.\\.+)$")){
+                    Pattern p = Pattern.compile("^(\\w+)(\\.\\.+)$");
+                    Matcher m = p.matcher(tokenStr);
+                    m.find();
+
+                    currentSentence.getTokens().add( new Token(m.group(1)));
+                    currentSentence.getTokens().add( new Token(m.group(2)));
+                    if (currentSentence.getTokens().size() > 0){
+                        sentences.add(currentSentence);
+                        currentSentence = new Sentence();
+                    }
+
+                } else if (tokenStr.matches("^(\\w+)([,])(\\.)$")){
+                    Pattern p = Pattern.compile("^(\\w+)([,]+)(\\.)$");
+                    Matcher m = p.matcher(tokenStr);
+                    m.find();
+
+                    currentSentence.getTokens().add( new Token(m.group(1)));
+                    currentSentence.getTokens().add( new Token(m.group(2)));
+                    currentSentence.getTokens().add( new Token(m.group(3)));
+                    if (currentSentence.getTokens().size() > 0){
+                        sentences.add(currentSentence);
+                        currentSentence = new Sentence();
+                    }
+
+                } else if (tokenStr.matches("^(\\w+)([,])(\\.\\.+)$")){
+                    /*"word,..."*/
+                    Pattern p = Pattern.compile("^(\\w+)([,]+)(\\.\\.+)$");
+                    Matcher m = p.matcher(tokenStr);
+                    m.find();
+
+                    currentSentence.getTokens().add( new Token(m.group(1)));
+                    currentSentence.getTokens().add( new Token(m.group(2)));
+                    currentSentence.getTokens().add( new Token(m.group(3)));
+                    if (currentSentence.getTokens().size() > 0){
+                        sentences.add(currentSentence);
+                        currentSentence = new Sentence();
+                    }
+                }else if (tokenStr.matches("[\\.]+")){
+
+                    currentSentence.getTokens().add( new Token(tokenStr));
+                }else if (tokenStr.matches("[^\\.]+")){
+
+                    StringBuilder currentWord = new StringBuilder();
+                    for (int i = 0; i < tokenStr.length(); i++) {
+                        String currentLetter = tokenStr.substring(i,i+1);
+                        if ( currentLetter.matches("\\p{Punct}" )){
+                            if (currentWord.length() > 0){
+                                currentSentence.getTokens().add( new Token( currentWord.toString()  ));
+                                currentWord.setLength(0);
+                            }
+                            currentSentence.getTokens().add( new Token( currentLetter  ));
+
+                        } else {
+                            currentWord.append(currentLetter);
+                        }
+
+
+                    }
+                    if (currentWord.length() > 0){
+                        currentSentence.getTokens().add( new Token( currentWord.toString()  ));
+                        currentWord.setLength(0);
+                    }
+
+                }
+
+                if ( reachedEOS || tokenStr.matches("\\.") ){
+                    if (currentSentence.getTokens().size() > 0){
+                        sentences.add(currentSentence);
+                        currentSentence = new Sentence();
+                    }
+
+                }
+            }
+            if (currentTokenType != TokenType.WHITESPACE ) {
+                currentToken.append(c);
+            }
+            lastTokenType = currentTokenType;
 
             data = inputStreamReader.read();
         }
 
-        if  (lastTokenType.isTokenizeAble() ) {
-            if (lastTokenType != TokenType.DOT  ||
-                    lastTokenType == TokenType.DOT && currentToken.length() > 1){
-                saveNonZeroLengthTokens(currentSentence, currentToken);
-            }
-        }
-        if (currentSentence.getTokens().size() > 0){
-            sentences.add(currentSentence);
-        }
 
 
         inputStreamReader.close();
@@ -111,6 +188,9 @@ public class BasicParser {
 
         return sentences;
     }
+
+
+
     private void saveNonZeroLengthTokens(Sentence currentSentence, StringBuilder currentToken) {
         if (currentToken.length() > 0) {
 
--- src/test/java/com/digitalreasoning/parser/BasicParserTest.java
+++ src/test/java/com/digitalreasoning/parser/BasicParserTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -21,23 +22,92 @@ public class BasicParserTest {
     XmlSerializer serializer = new BasicXMLSerializer();
 
 
+
+    int maxPhraseLength,maxActualLength,maxExpectedLength;
+    class Result{
+        String phrase, expected, result;
+
+
+    }
+    List<Result> results = new ArrayList<Result>();
+    public void validate(String phrase, String expected){
+        Result r = new Result();
+        r.phrase= phrase;
+        r.expected = expected;
+
+        try {
+            r.result =  serializer.serializeSentences( parser.parseString(phrase) );
+        } catch (IOException e) {
+            r.result = "Exception";
+            e.printStackTrace();
+        }
+        maxPhraseLength = Math.max(maxPhraseLength,r.phrase.length() + 2);
+        maxActualLength = Math.max(maxActualLength,r.result.length() + 2);
+        maxExpectedLength = Math.max(maxExpectedLength,r.expected.length() + 2);
+
+        results.add(r);
+    }
+    public void printResults(){
+        int failureCnt = 0;
+        for (Result r :results){
+            String formattedPhrase   = String.format("%-" + maxPhraseLength   + "s"  , "\""+r.phrase   + "\""  );
+            String formattedExpected = String.format("%-" + maxExpectedLength + "s"  , "\""+r.expected + "\""  );
+            String formattedActual   = String.format("%-" + maxActualLength   + "s"  , "\""+r.result   + "\""  );
+
+            if ( !r.expected.equals( r.result) ){
+                System.out.println(String.format("Failure %s:   Expected:%s  Actual:%s ", formattedPhrase, formattedExpected,formattedActual));
+                failureCnt++;
+            } else{
+                System.out.println(String.format("Success %s:   Expected:%s ", formattedPhrase, formattedExpected,formattedActual) );
+            }
+
+        }
+
+        if (failureCnt > 0){
+            fail( String.format( "There were %s failures", failureCnt));
+        }
+    }
+
     @Test
     public void parseFile_whenGivenTestFile_thenAllSymbolsAreTokenized() throws IOException {
-        validateSentenceParsing("word ."   , parser, /*Sentences=*/ 1, /*Total Tokens*/1);
-        validateSentenceParsing("word ..." , parser, /*Sentences=*/ 1, /*Total Tokens*/2);
-        validateSentenceParsing("word."    , parser, /*Sentences=*/ 1, /*Total Tokens*/1);
-        validateSentenceParsing("word..."  , parser, /*Sentences=*/ 1, /*Total Tokens*/2);
-        validateSentenceParsing("word,."   , parser, /*Sentences=*/ 1, /*Total Tokens*/2);
-        validateSentenceParsing("word,..." , parser, /*Sentences=*/ 1, /*Total Tokens*/3);
+        reset();
+        validate("word ."               ,          "<sentence><token>word</token><token>.</token></sentence>"                                      );
+        validate("word.cat"               ,        "<sentence><token>word.cat</token></sentence>"                     );
+        validate("word ..."             ,          "<sentence><token>word</token><token>...</token></sentence>"                 );
+        validate("word."                ,           "<sentence><token>word</token><token>.</token></sentence>"                                    );
+        validate("word..."              ,          "<sentence><token>word</token><token>...</token></sentence>"                 );
+        validate("word,."               ,          "<sentence><token>word</token><token>,</token><token>.</token></sentence>"                   );
+        validate("word,..."             ,          "<sentence><token>word</token><token>,</token><token>...</token></sentence>" );
+        validate("Euclid's"             ,          "<sentence><token>Euclid's</token></sentence>"                               );
+        validate("2.12"                 ,          "<sentence><token>2.12</token></sentence>"                                       );
+        validate("20th Century"         ,          "<sentence><token>20th</token><token>Century</token></sentence>"                                       );
+        validate("(BFGS)"               ,          "<sentence><token>(</token><token>BFGS</token><token>)</token></sentence>"                                       );
+        validate("340x180x90"           ,          "<sentence><token>340x180x90</token></sentence>"                                       );
+        validate("340 x 180 x 90"       ,          "<sentence><token>340</token><token>x</token><token>180</token><token>x</token><token>90</token></sentence>"                                       );
 
-        validateSentenceParsing("word\n ."   , parser, /*Sentences=*/ 1, /*Total Tokens*/1);
-        validateSentenceParsing("word\n ..." , parser, /*Sentences=*/ 1, /*Total Tokens*/2);
-        validateSentenceParsing("word\n."    , parser, /*Sentences=*/ 1, /*Total Tokens*/1);
-        validateSentenceParsing("word\n..."  , parser, /*Sentences=*/ 1, /*Total Tokens*/2);
-        validateSentenceParsing("word\n,."   , parser, /*Sentences=*/ 1, /*Total Tokens*/2);
-        validateSentenceParsing("word\n,..." , parser, /*Sentences=*/ 1, /*Total Tokens*/3);
+        printResults();
 
 
+
+
+    }
+    @Test
+    public void testPunctuation() throws IOException {
+        reset();
+        validate("\"Word\""           ,         "<sentence><token>\"</token><token>Word</token><token>\"</token></sentence>"                                );
+        validate("\"Word"           ,         "<sentence><token>\"</token><token>Word</token></sentence>"                                );
+        validate("Word\""           ,         "<sentence><token>Word</token><token>\"</token></sentence>"                                );
+        validate("Word,"              ,         "<sentence><token>Word</token><token>,</token></sentence>"                                        );
+        validate(",Word"              ,         "<sentence><token>,</token><token>Word</token></sentence>"                                        );
+        validate("Word:,"             ,        "<sentence><token>Word</token><token>:</token><token>,</token></sentence>"                                        );
+        validate("',,,'"              ,          "<sentence><token>'</token><token>,</token><token>,</token><token>,</token><token>'</token></sentence>"                                        );
+
+        printResults();
+
+    }
+
+    private void reset() {
+        results.clear();
     }
 
     public void validateSentenceParsing(String str, BasicParser basicParser, int expectedSentences, int expectedTokens) throws IOException {
@@ -69,7 +139,7 @@ public class BasicParserTest {
         System.out.println( new BasicXMLSerializer().serializeSentences(sentences) );
 
         assertEquals( sentences.size(),2);
-        assertEquals( sentences.get(0).getTokens().size(),6);
+        assertEquals( sentences.get(0).getTokens().size(),7);
         assertEquals( sentences.get(1).getTokens().size(),3);
 
 
